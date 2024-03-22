8ec06e13c3d3596fc174ee5255ca03f20c5e9f9f
Junseok Oh <119016575+eDi9root@users.noreply.github.com>
2024-03-22 03:00:40 -0400

    Add files via upload

---
+++ portfolio web/assets/js/Type.js
@@ -0,0 +1,19 @@
+/*
+import React from "react";
+import Typewriter from 'typewriter-effect';
+
+function Type() {
+    return (
+        <Typewriter
+        options={{
+            strings: ['Hello', 'World'],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 40,
+        }}
+        />
+    );
+}
+
+export default Type;
+*/
